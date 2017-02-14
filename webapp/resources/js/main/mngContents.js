bf1d49cfcd034c5c7a0edd7eb904486ba75b3cee
devmrko <devmrko@gmail.com>
2017-02-14 11:46:59 +0900

    wysiwyg 추가

--- webapp/resources/js/main/mngContents.js
+++ webapp/resources/js/main/mngContents.js
@@ -1,9 +1,20 @@
-var obj_NgApp = angular.module('app_mngContents', [ ]);
-
-obj_NgApp.controller('ctr_mngContents', function($scope, $http, $document, $window, $q) {
+app.controller('ctr_mngContents', function($scope, $http, $document, $window, $q) {
 
 	$document.ready(function() {
+		$scope.sel_title = "테스트 타이틀";
+		
+//		$('#summernote').summernote({
+//        });
+		
+		
+		var wysiwyg_str = '<div class="col-md-12"><input class="btn btn-default" type="button" value="Save" ng-click="savePost()"/><input class="btn btn-default" type="button" value="Complete" ng-click="completeClick(sel_id)" ng-hide="!completeButtonBool"/><input class="btn btn-default" type="button" value="Cancel Completion" ng-click="cancelCompletionClick(sel_id)" ng-hide="completeButtonBool"/><input class="btn btn-default" type="button" value="Cancel" ng-click="cancleClickAtDetail()"/><input class="form-control" name="sel_id" type="text" ng-model="sel_id" ng-hide="true"/></div>';
 
+		$('#summernote').summernote('code', wysiwyg_str);
+		
 	});
 
+//	
+//	$("#modal-content,#modal-background").toggleClass("active");
+//	$("#modal-content,#modal-background").draggable();
+	
 });
